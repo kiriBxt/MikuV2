60462a83f894f749f6cecd9696c74755ff367f87
kiriBxt <kiri.vocg@gmail.com>
2023-07-01 12:11:38 +0200

    presence

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -1,8 +1,12 @@
-const fs = require("fs");
+const { ActivityType } = require("discord.js");
 module.exports = {
   name: "ready",
   once: true,
   async execute(client) {
     console.log(`ready ${client.user.tag} is logged`);
+    client.user.setPresence({
+      activities: [{ name: `24/7`, type: ActivityType.Playing }],
+      status: "dnd",
+    });
   },
 };
