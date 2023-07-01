499d04d919a71269ba66ca23dd3c79c920f88ce4
kiriBxt <kiri.vocg@gmail.com>
2023-07-01 13:39:38 +0200

    8ball und catsay

---
+++ src/commands/fun/_8ball.js
@@ -0,0 +1,52 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+
+module.exports = {
+  cooldown: 10,
+  data: new SlashCommandBuilder()
+    .setName("8ball")
+    .setDescription("Sagt die Zukunft an")
+    .addStringOption((option) =>
+      option.setName("input").setDescription("User input").setRequired(true)
+    ),
+  async execute(interaction, client) {
+    const string = interaction.options.getString("input");
+    var fortunes = [
+      "Ja!",
+      "Sicherlich.",
+      "Scheint so zu sein.",
+      "Ohne Zweifel.",
+      "Bestimmt.",
+      "Du kannst darauf vertrauen.",
+      "Ich denke schon.",
+      "Sehr Wahrscheinlich.",
+      "sieht gut aus.",
+      "Die Zeichen sehen gut aus.",
+      "Zu faul zum Antworten.",
+      "Versuchs später nochmal.",
+      "Besser du weisst es noch nicht...",
+      "Kann jetzt nichts sagen.",
+      "Konzentriere dich und frag nochmal.",
+      "Verlass dich nicht drauf.",
+      "Meine Anwtort ist nein.",
+      "Meine Quellen sagen nein.",
+      "Sieht nicht gut aus...",
+      "Sehr zweifelhaft.",
+    ];
+    const embed = new EmbedBuilder().setColor(0x18e1ee).addFields([
+      {
+        name: `**${interaction.user.username} fragt:**`,
+        value: `${string}`,
+        inline: true,
+      },
+      {
+        name: `**MikuV2:**`,
+        value: `${fortunes[Math.floor(Math.random() * fortunes.length)]}`,
+        inline: false,
+      },
+    ]);
+
+    await interaction.reply({
+      embeds: [embed],
+    });
+  },
+};
---
+++ src/commands/fun/catsay.js
@@ -0,0 +1,23 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+
+module.exports = {
+  cooldown: 10,
+  data: new SlashCommandBuilder()
+    .setName("catsay")
+    .setDescription("Lass eine katze für dich sprechen")
+    .addStringOption((option) =>
+      option.setName("input").setDescription("User input").setRequired(true)
+    ),
+  async execute(interaction, client) {
+    const string = interaction.options.getString("input");
+
+    await interaction.reply({
+      files: [
+        {
+          attachment: `https://cataas.com/cat/cute/says/${string}`,
+          name: "catsay.png",
+        },
+      ],
+    });
+  },
+};
