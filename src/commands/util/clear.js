773c60f5c8e6b7a3e0f861e8f833a43320a8e298
kiriBxt <kiri.vocg@gmail.com>
2023-07-13 20:29:57 +0200

    neue clear funktion

---
+++ src/commands/util/clear.js
@@ -0,0 +1,49 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
+const wait = require("node:timers/promises").setTimeout;
+
+module.exports = {
+  cooldown: 10,
+  data: new SlashCommandBuilder()
+    .setName("clear")
+    .setDescription("Löscht Nachrichten in einem Channel")
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+    .addIntegerOption((option) =>
+      option
+        .setName("amount")
+        .setDescription(
+          "Löscht eine anzahl an Nachrichten in einem channel. Diese Funktion ist Ratelimited"
+        )
+        .setMinValue(1)
+        .setMaxValue(100)
+        .setRequired(true)
+    ),
+  async execute(interaction) {
+    const number = interaction.options.getInteger("amount");
+    let msgDeleted = 0;
+    let messageId;
+    let msgArr = [];
+
+    const channel = interaction.channel;
+
+    const messages = await channel.messages.fetch({ limit: number });
+    messages.filter(
+      (message) => ((messageId = message.id), msgArr.push(message.id))
+    );
+    msgArr.forEach(async (msg) => {
+      try {
+        msgDeleted++;
+        await channel.messages.delete(msg);
+      } catch (e) {
+        return await interaction.reply({
+          content: `Error: ${e.rawError.message} `,
+          ephemeral: true,
+        });
+      }
+    });
+
+    await interaction.reply({
+      content: `${msgDeleted} will get deleted`,
+      ephemeral: true,
+    });
+  },
+};
