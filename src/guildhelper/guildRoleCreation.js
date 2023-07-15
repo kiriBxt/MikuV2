0c546887e400374f30b7f4602bac3f9dae86cc17
kiriBxt <kiri.vocg@gmail.com>
2023-07-15 21:52:21 +0200

    log edit

--- src/guildhelper/guildRoleCreation.js
+++ src/guildhelper/guildRoleCreation.js
@@ -1,6 +1,5 @@
 const guildRoleCreation = async (guild, roleName, roleColor) => {
   await guild.roles.create({ name: roleName, color: roleColor });
-  console.log("role " + roleName + " created!");
   return;
 };
 module.exports = guildRoleCreation;
