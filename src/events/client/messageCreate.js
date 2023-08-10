141982cec51920e704626207dd81547c5ee14081
kiriBxt <kiri.vocg@gmail.com>
2023-08-10 16:20:11 +0200

    bal und xp update

--- src/events/client/messageCreate.js
+++ src/events/client/messageCreate.js
@@ -4,7 +4,7 @@ module.exports = {
   name: "messageCreate",
   async execute(message) {
     if (message.author.bot) return;
-    await addXp(message.author, 10, 50);
-    await addBal(message.author, 50, 200);
+    await addXp(message.author, 10, 30);
+    await addBal(message.author, 25, 100);
   },
 };
